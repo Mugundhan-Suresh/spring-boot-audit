7f4e6a0fc9ee62ed734823c875d91659c07e7f27
Mugundhan-strugglebreaker <srmvmugund@gmail.com>
2025-03-01 11:05:00 +0530

    Initiaze the spring boot project

---
+++ src/main/java/com/mugundhan/spring_audit/spring_audit/SpringAuditApplication.java
@@ -0,0 +1,13 @@
+package com.mugundhan.spring_audit.spring_audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mugundhan/spring_audit/spring_audit/SpringAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mugundhan.spring_audit.spring_audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
